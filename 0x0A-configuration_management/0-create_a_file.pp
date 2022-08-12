861f21f943d5e737e7abbb55c440dc66180ad90a
Dak4rPrO <98028958+Dak4rPrO@users.noreply.github.com>
2022-08-12 16:08:14 -0300

    errores boludos

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Using Puppet, create a file in /tmp.
 file { '/tmp/school':
-  owner   => www-date,
-  group   => www-date,
+  owner   => www-data,
+  group   => www-data,
   mode    => '0744',
   content => 'I love Puppet'
   }
