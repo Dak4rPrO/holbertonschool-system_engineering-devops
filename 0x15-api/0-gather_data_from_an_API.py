9348e4ac149080aee40e7ff180fbbf53bfc89113
Dak4rPrO <98028958+Dak4rPrO@users.noreply.github.com>
2022-09-06 17:15:22 -0300

    terminar en casa

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,17 +9,17 @@ import sys
 
 if __name__ == "__main__":
     
-    def ident(user_id=int):
-        
-        user_id = sys.argv[1]
-        users = requests.get('https://jsonplaceholder.typicode.com/users/' + user_id)
-        data_users = users.json()
-        value_name = data_users.get('name')
+    user_id = sys.argv[1]
+    users = requests.get('https://jsonplaceholder.typicode.com/users/' + user_id)
+    data_users = users.json()
+    value_name = data_users.get('name')
 
-        list_todos = requests.get('https://jsonplaceholder.typicode.com/todos/' + user_id)
-        for index in list_todos.items():
-            list_dicts = list_todos.json()
-        value_n_task = list_dicts.get('completed')
-        
-        
-        return "Employee {value_name} is done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):".fortmat(value_name, Ntasks, Ttasks)
+    list_todos = requests.get('https://jsonplaceholder.typicode.com/todos/')
+    for index in list_todos.items():
+        list_dicts = list_todos.json()
+    value_n_task = list_dicts.get('completed')
+    trues = 0
+    falses = 0
+    titles = []
+    print "Employee {value_name} is done with tasks({Ntasks}/{Ttasks}):".fortmat(value_name, Ntasks, Ttasks)
+            
