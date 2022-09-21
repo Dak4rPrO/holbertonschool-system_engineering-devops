d531bf5b61a61c10132d7361d0a04f9b25886953
Dak4rPrO <98028958+Dak4rPrO@users.noreply.github.com>
2022-09-21 18:06:36 -0300

    probando checks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,18 +2,14 @@
 """ function that queries the Reddit API and returns the number of subscribers
     (not active users, total subscribers) """
 
-import json
 import requests
-from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """ return the number of subscribers
-    (not active users, total subscribers)"""
+    user_agent = {'User-agent': 'Mozilla/5.0'}
     try:
-        user_agent = {'User-agent': 'Mozilla/5.0'}
-        subreddit = requests.get('https://www.reddit.com/r/{}/about.json'
-                                 .format(subreddit), headers=user_agent)
-        return (subreddit.json()['data']['subscribers'])
+        r = requests.get('https://www.reddit.com/r/{}/about.json'
+                         .format(subreddit), headers=user_agent)
+        return r.json()['data']['subscribers']
     except Exception:
         return 0
