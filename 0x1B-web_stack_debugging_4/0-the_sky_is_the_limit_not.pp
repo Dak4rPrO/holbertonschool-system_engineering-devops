444db39337d8fc3b5f2e785bf88b1acf5ec68354
Dak4rPrO <98028958+Dak4rPrO@users.noreply.github.com>
2022-09-26 15:47:33 -0300

    task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,9 @@
 #Using puppet, find out why requests are failing and fix them.
 
-exec {'sed -i s/15/1000/ /etc/default/nginx':}
+exec {
+  'sed -i s/15/1000/ /etc/default/nginx':
+}
 
-exec {'service nginx restart':}
+exec {
+  'service nginx restart':
+}
